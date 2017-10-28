4a30371b2aa90a9f1761091b5e31cabe54e4ad96
Vladimir Prikhodko <con8@mail.ru>
2017-10-28 17:27:33 +0300

    Работа с шаблонизатором вариация мест работы, года рождения, имени и фамилии

--- main_app/views.py
+++ main_app/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render_to_response
 
 # Create your views here.
 def main(request):
-	return(render_to_response("index.html"))
+	age = 1988
+	name = 'Владимир'
+	second_name = 'Приходько'
+	return render_to_response('index.html', {'age':age,'name':name, 'second_name':second_name})
 def study(request):
-	return(render_to_response("study.html"))
+	return render_to_response("study.html")
 def work(request):
-	return(render_to_response("work.html"))
+	place_work = ['Google','Samsung', 'Mail.ru', 'ООО "Вэб студия"']
+	return render_to_response('work.html', {'place_work': place_work})
 def bonus(request):
-	return(render_to_response("bonus.html"))
+	return render_to_response("bonus.html")
