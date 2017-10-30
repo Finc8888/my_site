5bd3edb9159e9b37208d920c896144594d785cf6
finc8888 <con8@mail.ru>
2017-10-30 15:40:58 +0300

    Второе ДЗ: наследование шаблонов, фильтры шаблонных тегов

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-
+import datetime
 # Create your views here.
 def main(request):
-	age = 1988
-	name = 'Владимир'
+	hobby = ['программирование','радиодело', 'занятия спортом']
+	age = datetime.date(1988,3,8)
+	name = 'владимир'
 	second_name = 'Приходько'
-	return render_to_response('index.html', {'age':age,'name':name, 'second_name':second_name})
+	return render_to_response('index.html', {'hobby': hobby,'age':age,'name':name, 
+		'second_name':second_name, })
 def study(request):
 	return render_to_response("study.html")
 def work(request):
-	place_work = ['Google','Samsung', 'Mail.ru', 'ООО "Вэб студия"']
+	place_work = ['Google','Samsung', 'Mail.ru', 'Яндекс']
 	return render_to_response('work.html', {'place_work': place_work})
 def bonus(request):
-	return render_to_response("bonus.html")
+	value = datetime.datetime.now()
+	return render_to_response("bonus.html", {'value': value})
